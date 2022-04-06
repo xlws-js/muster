e8175ade245acf92f076bc439a169da30b65cbab
xl <xl@qq.com>
2022-04-06 17:17:40 +0800

    set theme

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,15 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import routes from 'virtual:generated-pages'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import "@unocss/reset/tailwind.css"
+import './style/main.css'
+import 'uno.css'
+
+createApp(App).use(
+    createRouter({
+        history: createWebHistory(import.meta.env.BASE_URL),
+        routes
+    })
+).mount('#app')
